cdd00aa202acc4d5cffd846805bd7f4a16f17f82
David-Solodukhin <david.solodukhin@gmail.com>
2018-03-11 20:13:13 -0400

    startui basics

--- src/com/company/Listener.java
+++ src/com/company/Listener.java
@@ -202,9 +202,14 @@ public class Listener extends Thread{
             System.exit(1);
             return;
         }
+        /*
+        Since every update is an rtt_table, we don't need the specific ip and port of the sender. we just merge.
+         */
+
         Ringo.rtt_table.merge(tmp);
         if (Ringo.rtt_table.isComplete()) {//inefficient but whatever, can technically move this so it's not o(2n) but o(n) before
             //call optimal ring formation method
+            formOptimalRing();
         }
 
 
@@ -249,6 +254,7 @@ public class Listener extends Thread{
      */
     public void formOptimalRing() {
         int[][] converted = Ringo.rtt_table.convert();
+        Ringo.rtt_converted = converted;
         int[] ringRaw = getShortestHamiltonianCycle(converted);
         String[] ipRing = new String[ringRaw.length];
         int i = 0;
@@ -258,6 +264,8 @@ public class Listener extends Thread{
             ipRing[i] = ip;
             i++;
         }
+        Ringo.optimalRing = ipRing;
+        Ringo.startUI();
         /*
         TODO: figure out what to do with this string array which represents the optimal ring and let other nodes know you're done?
         edge cases: somehow some nodes don't come up with the same optimal ring?
